e2bd45323a044e930411a8662a572af6c31ae9df
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-05-10 17:54:04 +0000

    abcd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
+
+// const root = ReactDOM(document.getElementById('root'));
+
+// root.render(<App/>)
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
-const root = ReactDOM(document.getElementById('root'));
-
-root.render(<App/>)
-
+ReactDOM.render(<App/> , document.getElementById("root"));
